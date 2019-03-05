fbfdc5f11f51e4899f663a3d2df331360e89450a
Unknown <CSnow040@west-mec.org>
2019-03-04 18:07:21 -0700

    Finished JavaScript except small problem
    I haven't fixed this minor problem in the javascript and I haven't added comments yet

--- hg_report.js
+++ hg_report.js
@@ -13,11 +13,26 @@
 */
 
 var gameReport =
-    "<h1>" + itemTitle + "</h1> <h2>By:" + itemManufacturer + "</h2> <img src='hg_" + itemID + ".png' alt=id id=gameImg /> <table><tr><th>Product ID</th><td>id</td></tr> <tr><th>" + itemPrice + "</th><td>price</td></tr><tr><th>" + itemPlatform + "</th><td>platform</td></tr> <tr><th>" + itemESRB + "</th><td>esrb</td></tr> <tr><th>" + itemCondition +
-    "</th><td>condition</td> </tr><tr><th>" + itemRelease + "</th><td>release</td></tr></table>" + itemSummary;
+    "<h1>" + itemTitle + "</h1> <h2>By:" + itemManufacturer + "</h2> <img src='hg_" + itemID + ".png' alt=id id=gameImg /> <table><tr><th>Product ID</th><td>" + itemID + "</td></tr> <tr><th>List Price</th><td>" + itemPrice + "</td></tr><tr><th>Platform</th><td>" + itemPlatform + "</td></tr> <tr><th>ESBR Rating</th><td>" + itemESRB + "</td></tr> <tr><th>Condition</th><td>" + itemCondition + "</td> </tr><tr><th>Release</th><td>" + itemRelease + "</td></tr></table>" + itemSummary;
 
-document.getElementById("article").innerHTML = gameReport;
+document.getElementsByTagName("article")[0].innerHTML = gameReport;
 
-var ratingsSum = 0;
+var ratingSum = 0;
+var ratingCount = ratings.length;
 
-var ratingsCount = ratings.length;
+for (var i = 0; i = ratingSum; ratingCount + ratingSum) {
+    var ratingAvg = ratingSum / ratingsCount;
+    var ratingReport = "<h1>Customer Reviews</h1><h2>" + ratingAvg + "out of 5 stars" + ratingCount + "</h2>";
+}
+
+for (var i = 0; i <= 2; i++) {
+    var ratingReport = "<div class='review'> <h1>" + ratingTitles + "</h1><table> <tr><th>By</th><td>" + ratingAuthors + "</td></tr> <tr><th>Review Date</th><td>" + ratingDates + "</td></tr> <tr><th>Rating</th><td>";
+
+    for (var i = 1; i <= 5; i++) {
+        var ratingReport = "<img src='hg_star.png' />";
+    }
+
+    var ratingReport = "</td></tr></table>" + ratingSummaries + "</div>";
+}
+
+document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
